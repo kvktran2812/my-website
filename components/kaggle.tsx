071b8104207afb7320918ed99ec4fdfb027cd3aa
kvktran2812 <kvktran@gmail.com>
2025-01-09 16:30:56 -0500

    fix bugs again

--- components/kaggle.tsx
+++ components/kaggle.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styles from "./css/Kaggle.module.css";
-import Image from "next/image";
-import Card from 'react-bootstrap/Card';
 
 
 export default function Kaggle() {
