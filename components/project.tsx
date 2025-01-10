9c1f6d8fdfc853dd75906179f4a1be575153d960
kvktran2812 <kvktran@gmail.com>
2025-01-10 13:07:15 -0500

    update info

--- components/project.tsx
+++ components/project.tsx
@@ -2,32 +2,29 @@ import React, { ReactComponentElement } from 'react'
 import 'react-vertical-timeline-component/style.min.css';
 import styles from "./css/Project.module.css";
 import Image from 'next/image';
+import { link } from 'fs';
 
 
 const ProjectPage = () => {
   const projects = [
     {
-      title: "Project 1",
-      img: "",
+      title: "GPT from scratch for six-eight poem",
+      link: "https://github.com/kvktran2812/scratch-torch-gpt",
+      img: "/img/project1.png",
       desc: 
       <div>
-        Some description
+        In this project I built a GPT model from scratch using Pytorch to generate six-eight style poem (a type of poetry in Vietnam). 
+        All components like single-head attention, multi-head attention, and feed forward are implemented from scratch.
       </div>,
     },
     {
-      title: "Project 1",
-      img: "",
+      title: "Financial news data pipeline",
+      link: "https://github.com/kvktran2812/financial-news",
+      img: "/img/project2.png",
       desc: 
       <div>
-        Some description
-      </div>,
-    },
-    {
-      title: "Project 1",
-      img: "",
-      desc: 
-      <div>
-        Some description
+        This is a data pipeline that I built to collect news data from multiple sources using web scraping techniques.
+        The data is then stored in a database and can be later used for analysis and machine learning tasks.
       </div>,
     },
   ]
@@ -38,9 +35,14 @@ const ProjectPage = () => {
       {projects.map((project, index) => (
           <div key={index} className={styles.projectItem}>
             <div className={styles.icon}>
+              <a href="https://github.com/kvktran2812/scratch-torch-gpt">
               <Image src={project.img} alt={project.title} width={100} height={100} />
+              </a>
             </div>
-            <div className={styles.desc}>{project.desc}</div>
+            <div className={styles.desc}>
+              <p><b>{project.title}</b></p>
+              {project.desc}
+            </div>
           </div>
       ))}
     </div>
