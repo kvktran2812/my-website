ceaf4adfa5db6cdefdf0e6c0109dce8b9d1df4c1
kvktran2812 <kvktran@gmail.com>
2025-01-29 23:59:16 -0500

    update website

--- components/deepLearningConcepts.tsx
+++ components/deepLearningConcepts.tsx
@@ -7,27 +7,59 @@ import Image from 'next/image';
 const DeepLearningPage = () => {
   const concepts = [
     {
-      title: "Project 1",
-      img: "",
+      title: "AlexNet - Krizhevsky, A., Sutskever, I., & Hinton, G. E. (2012)",
+      link: "https://github.com/kvktran2812/computer-vision/blob/main/scripts/alex_net.py",
       desc: 
       <div>
-        Some description
+        Very old school model for image classification. Proof of concept that deep convolutional networks can significantly outperform traditional machine learning methods on large-scale image datasets
       </div>,
     },
     {
-      title: "Project 1",
-      img: "",
+      title: "Inception model - Christian Szegedy et al, 2014",
+      link: "https://github.com/kvktran2812/computer-vision/blob/main/scripts/res_net.py",
       desc: 
       <div>
-        Some description
+        InceptionNet was introduced in 2014 and won the ImageNet Challenge (ILSVRC-2014) by significantly improving computational efficiency and accuracy. Unlike traditional CNNs, it introduced the Inception module, which uses multi-scale convolutions in parallel to extract features at different levels.
       </div>,
     },
     {
-      title: "Project 1",
-      img: "",
+      title: "ResNet - He et al., 2015",
+      link: "https://github.com/kvktran2812/computer-vision/blob/main/scripts/res_net.py",
       desc: 
       <div>
-        Some description
+        ResNet revolutionized deep learning by introducing residual connections to address the vanishing gradient problem in very deep networks
+      </div>,
+    },
+    {
+      title: "InceptionV4 and Inception ResNet V2 models - Christian Szegedy et al, 2016",
+      link: "https://github.com/kvktran2812/computer-vision/blob/main/scripts/inception_v4.py",
+      desc: 
+      <div>
+        Inception-v4 is an enhanced version of the Inception architecture, incorporating residual connections to improve training efficiency. Training very deep model with multiple conv layers.
+      </div>,
+    },
+    {
+      title: "MobileNet- Andrew G. Howard et al, 2017",
+      link: "https://github.com/kvktran2812/computer-vision/blob/main/scripts/mobile_net.py",
+      desc: 
+      <div>
+        MobileNet is a lightweight deep learning architecture optimized for mobile and embedded vision applications. Introduced in 2017, it uses depthwise separable convolutions to reduce the number of parameters and computational cost while maintaining high accuracy
+      </div>,
+    },
+    {
+      title: "SENet - Jie Hu et al, 2018",
+      link: "https://github.com/kvktran2812/computer-vision/blob/main/scripts/se_net.py",
+      desc: 
+      <div>
+        MobileNet is a lightweight deep learning architecture optimized for mobile and embedded vision applications. Introduced in 2017, it uses depthwise separable convolutions to reduce the number of parameters and computational cost while maintaining high accuracy
+      </div>,
+    },
+    {
+      title: "EfficientNet - Mingxing Tan and Quoc V. Le",
+      link: "https://github.com/kvktran2812/computer-vision/blob/main/scripts/efficient_net.py",
+      desc: 
+      <div>
+        EfficientNet is a family of CNN models designed to optimize accuracy and efficiency by using a novel compound scaling method.
       </div>,
     },
   ]
@@ -38,17 +70,14 @@ const DeepLearningPage = () => {
       <p>These are some of the deep learning famous models, architectures, and algorithms that I re-implemented from scratch from multiple research papers.
         Often when I learn new deep learning knowledge, I try to replicate it to understand how it works from the smallest detail.
       </p>
-      <p>Not implemented yet</p>
-      {/* {concepts.map((concept, index) => (
+      {concepts.map((concept, index) => (
           <div key={index} className={styles.projectItem}>
-            <div className={styles.icon}>
-              <Image src={concept.img} alt={concept.title} width={100} height={100} />
-            </div>
             <div className={styles.desc}>
+              <a href={concept.link}><b>{concept.title}</b></a>
               {concept.desc}
             </div>
           </div>
-      ))} */}
+      ))}
     </div>
   )
 }
--- components/project.tsx
+++ components/project.tsx
@@ -8,13 +8,23 @@ import { link } from 'fs';
 const ProjectPage = () => {
   const projects = [
     {
-      title: "GPT from scratch for six-eight poem",
+      title: "Computer Vision Legacy",
+      link: "https://github.com/kvktran2812/computer-vision",
+      img: "/img/project3.png",
+      desc: 
+      <div>
+        This repository contains my code for computer vision tasks, I re-implement famous legacy architectures, papers, and models to learn how those
+        technologies work. All those models are built from scratch with torch and Python. 
+      </div>,
+    },
+    {
+      title: "Trueyn-Kieu-GPT",
       link: "https://github.com/kvktran2812/scratch-torch-gpt",
       img: "/img/project1.png",
       desc: 
       <div>
         In this project, I built a GPT model from scratch using Pytorch to generate six-eight style poem (a type of poetry in Vietnam). 
-        All components like single-head attention, multi-head attention, and feed forward are implemented from scratch.
+        All components like single-head attention, multi-head attention, and feed forward are implemented from scratch. The model learn Vietnamese language through Truyen Kieu dataset
       </div>,
     },
     {
@@ -40,6 +50,7 @@ const ProjectPage = () => {
               </a>
             </div>
             <div className={styles.desc}>
+              <p><b>{project.title}</b></p>
               {project.desc}
             </div>
           </div>
